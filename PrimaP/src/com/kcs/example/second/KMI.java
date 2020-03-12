6fc6419ccf3689ee2a911c1fd799a8829c8a41d2
RimasZuk <rimas.zukauskas1@gmail.com>
2020-03-12 15:28:27 +0200

    KMI v1

--- PrimaP/src/com/kcs/example/second/KMI.java
+++ PrimaP/src/com/kcs/example/second/KMI.java
@@ -1,10 +1,21 @@
 package com.kcs.example.second;
 
+import java.sql.SQLOutput;
 import java.util.Scanner;
 
 public class KMI {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+    }
+
+    private static void ivedimas(Scanner sc) {
+        System.out.println("Iveskite savo mase (kg): ");
+        System.out.println("Iveskite, koks tavo ugis (m)");
+    }
+
+    private static double skaiciavimas(double kilo, double ugis) {
+        double kmi = kilo / Math.pow(ugis, 2);
+        return kmi;
     }
 
 
