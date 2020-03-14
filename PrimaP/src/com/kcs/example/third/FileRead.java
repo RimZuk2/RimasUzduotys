a1ff2c9b75a73e86b828caf5a59e551cc780bde6
RimasZuk <rimas.zukauskas1@gmail.com>
2020-03-14 16:50:57 +0200

    Nuskaityti

---
+++ PrimaP/src/com/kcs/example/third/FileRead.java
@@ -0,0 +1,12 @@
+package com.kcs.example.third;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+
+public class FileRead {
+    public static void main(String[] args) {
+        BufferedReader bf = new BufferedReader(new FileReader("cars.txt"));
+        String line;
+        while ()
+    }
+}
--- PrimaP/src/com/kcs/example/third/ReaderFromFile.java
+++ PrimaP/src/com/kcs/example/third/ReaderFromFile.java
@@ -1,22 +1,27 @@
-package com.kcs.example.third;
-
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-
-public class ReaderFromFile {
-    public static void main(String[] args) {
-        PenktaUzduotis penktaUzduotis = new PenktaUzduotis();
-        File file = new File("cars.txt");
-        BufferedReader bf = new BufferedReader(new FileReader(file));
-        String line;
-        String carName = "";
-        double bestCarAverage = 0;
-        while (line = bf.readLine() != 0){
-            String[] elements = line.split(" ");
-
-
-        }
-
-    }
-}
+//package com.kcs.example.third;
+//
+//import java.io.BufferedReader;
+//import java.io.File;
+//import java.io.FileReader;
+//
+//public class ReaderFromFile {
+//    public static void main(String[] args) {
+//        PenktaUzduotis penktaUzduotis = new PenktaUzduotis();
+//        File file = new File("cars.txt");
+//        BufferedReader bf = new BufferedReader(new FileReader(file));
+//        String line;
+//        String carName = "";
+//        double bestCarAverage = 0;
+//        while (line = bf.readLine() != 0) {
+//            String[] elements = line.split(" ");
+//            double average = penktaUzduotis.average(Double.parseDouble(elements[1]), Double.parseDouble(elements[2]));
+//            if (bestCarAverage == 0 || bestCarAverage > average){
+//                bestCarAverage == average;
+//                carName = elements[0];
+//            }
+//            System.out.println(elements[0] + " " + average);
+//
+//        }
+//        System.out.println("Optimaliausias automobilis" + carName + " " + bestCarAverage);
+//    }
+//}
